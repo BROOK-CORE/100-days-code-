e8ae215b9c385681c3fbf959afe6b1ab47d344e8
BROOK-CORE <iambrook2007@gmail.com>
2025-10-14 00:42:54 +0530

    day13.c

--- day13.c
+++ day13.c
@@ -52,3 +52,23 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the value of n: ");
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Error: Please enter a positive integer.\n");
+        return 1;
+    }
+
+    printf("Numbers from 1 to %d:\n", n);
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    return 0;
+}
+
